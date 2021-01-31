adc442698d46a70a863daa551d2a39994189ebde
German Espinosa <germanespinosa@gmail.com>
2021-01-31 10:57:40 -0600

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/maze_cv_stitching_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for maze_cv_stitching_tests.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /maze/maze_cv_stitching
-# Build directory: /maze/maze_cv_stitching/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(maze_cv_stitchingTests ".tests/maze_cv_stitching_tests")
-set_tests_properties(maze_cv_stitchingTests PROPERTIES  _BACKTRACE_TRIPLES "/usr/local/share/CatchTests/catchtests.cmake;19;add_test;/usr/local/share/CatchTests/catchtests.cmake;0;;/usr/local/share/cmake/CatchTests/CatchTestsConfig.cmake;91;include;/usr/local/share/cmake/CatchTests/CatchTestsConfig.cmake;0;;/maze/maze_cv_stitching/CMakeLists.txt;41;find_package;/maze/maze_cv_stitching/CMakeLists.txt;0;")
