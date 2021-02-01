405ec2d2fc7ff7dfcdf38abd354da02c34becf98
germanespinosa <germanespinosa@gmail.com>
2021-01-31 22:25:48 -0600

    none

--- include/cleaner.h
+++ include/cleaner.h
@@ -3,7 +3,10 @@
 
 namespace habitat_cv{
     struct Cleaner {
+        Cleaner(unsigned int threshold = 50, unsigned int erosions = 2);
         cv::Mat &clean (cv::Mat &subtracted);
+        unsigned int threshold;
+        unsigned int erosions;
         cv::Mat clean_image;
     };
 }
--- src/cleaner.cpp
+++ src/cleaner.cpp
@@ -3,12 +3,18 @@
 namespace habitat_cv{
 
     cv::Mat &Cleaner::clean(cv::Mat &subtracted) {
-        cv::Mat bw = subtracted > 50;
+        cv::Mat bw = subtracted > threshold;
         cv::Mat eroded;
-        cv::erode(bw, eroded, cv::Mat(), cv::Point(-1, -1), 2, 1, 1);
+        cv::erode(bw, eroded, cv::Mat(), cv::Point(-1, -1), erosions, 1, 1);
         cv::Mat dilated;
-        cv::dilate(eroded, dilated, cv::Mat(), cv::Point(-1, -1), 4, 1, 2);
-        cv::erode(dilated, clean_image, cv::Mat(), cv::Point(-1, -1), 2, 1, 1);
+        cv::dilate(eroded, dilated, cv::Mat(), cv::Point(-1, -1), erosions*2, 1, 2);
+        cv::erode(dilated, clean_image, cv::Mat(), cv::Point(-1, -1), erosions, 1, 1);
         return clean_image;
+    }
+
+    Cleaner::Cleaner(unsigned int threshold, unsigned int erosions):
+    threshold(threshold),
+    erosions(erosions){
+
     }
 }
